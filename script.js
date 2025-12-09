af254b960e63ceb9fc4b473cccc601165e710958
Shoaib Khan <shoaib7878687@gmail.com>
2025-12-09 17:20:26 +0500

    updated changes

--- script.js
+++ script.js
@@ -2,5 +2,93 @@
 // Question No 1:
 
 
-var name="shoaib";
-alert(name);
+// var name="shoaib";
+// alert(name);
+
+// var num='1';
+// alert(num);
+
+// var name="khan";
+// var names="shoaib" + name;
+// alert(names);
+
+
+// Question No 2:
+
+// var username="Syed Shoaib Hussain Shah";
+// var messege="Welcome";
+// alert(messege);
+// console.log(username);
+// var name = "shoaib" + "khan";
+// console.log(name);
+
+
+// var city="karachi";
+// alert("Welcome to city");
+// console.log(city);
+
+//Question No 03 :
+
+// var age="19";
+// console.log(age);
+// var sum=15+20;
+// console.log(sum);
+// var subtract=2%22;
+// console.log(subtract);
+// var multiply=3*3;
+// console.log(multiply);
+// var messege="you are 20 years old";
+// console.log(messege);
+
+//Question No 04;
+
+// var x=20;
+// var y=30;
+
+// console.log(x*y);
+// console.log(x%y);
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x/y);
+
+//Question No 05;
+
+// var name=prompt("Enter your name:");
+// console.log(name);
+
+// var user=prompt("what is your age:");
+// console.log(user);
+
+// var fav=prompt("Ask the fav color:");
+// var color="wow i like " + fav +  " to ";
+// console.log (color);
+
+//Question No 06:
+
+// var age= prompt("Enter your age:");
+
+// if(age>30){
+//     console.log("your still young");
+// }
+// else{
+//     console.log("your still child");
+// }
+
+
+// var num=Number(prompt("Enter the num:"));
+
+// if(num % 2 == 0){
+//     console.log("Even num");
+// }
+// else{
+//     console.log("odd num");
+// }
+
+var pass= prompt("Enter your password:");
+var passwords="shoaibkhan";
+if(pass == passwords ){
+    console.log("Access granted");
+}
+else{
+    console.log("Access doesnot granted");
+}
